3965d83ee1bfdf50e3c3309cfcd289cfde405483
Yegor21 <egorossipik@mail.ru>
2022-06-28 19:54:27 +0300

    Initial commit

---
+++ src/SchoolManager.java
@@ -0,0 +1,20 @@
+public class SchoolManager {
+
+    public class Schoolmanager {
+        public static double calculateAvgMark(int[][] groups) {
+            double sum = 0;
+
+            for (int i = 0; i < groups.length; i++) {
+                sum += calculateGroupAvgMark(groups[i]);
+            }
+            return sum / groups.length;
+        }
+        private static double calculateGroupAvgMark(int[] group) {
+            double sum = 0;
+            for (int i = 0; i < group.length; i++) {
+                sum += group[i];
+            }
+            return sum / group.length;
+        }
+    }
+}
